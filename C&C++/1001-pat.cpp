15666947fb17c552b1f3175de4aa3a89642e5a1b
trashinkgit <xiaoliang@trashink.com>
2019-04-28 01:08:00 +0800

    first commit

---
+++ C&C++/1001-pat.cpp
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int f1(int x){
+	x=x/2;
+	return x;
+}
+int main(){
+	int a,sum=0;
+	scanf("%d",&a);
+	while(1){
+		if(a%2==0){
+			a=f1(a);
+			sum++;
+		}else{
+			a=f1(3*a+1);
+			sum++;
+		}
+		if(a==1)break;
+	}
+	printf("%d\n",sum);
+	return 0;
+} 
